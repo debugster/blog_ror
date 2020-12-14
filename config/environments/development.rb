89958ef33e202ad0a4777aed7fe572d7e2297a14
Nayeem Mahmood <alive.dew@gmail.com>
2020-12-14 13:32:49 +0600

    devise integrated

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,4 +62,7 @@ Rails.application.configure do
 
   # BetterErrors::Middleware.allow_ip! ENV['TRUSTED_IP'] if ENV['TRUSTED_IP']
   # BetterErrors::Middleware.allow_ip! '::1'
+
+  # Devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
