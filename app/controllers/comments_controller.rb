80948ed13e1a266891186adb2a2b589dcd71f954
Nayeem Mahmood <alive.dew@gmail.com>
2020-12-15 12:43:58 +0600

    user authenticated before deleting comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,20 @@
 class CommentsController < ApplicationController
+  before_action :find_post, only: [:create, :destroy]
+  before_action :authenticate_user!, only: [:create, :destroy]
 
   def create
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(comment_params)
-    redirect_to post_path(@post)
+    # @comment = @post.comments.create(comment_params)
+    @comment = @post.comments.new(comment_params)
+    @comment.user_id = current_user.id
+    @comment.save
+    redirect_to @post
   end
 
   def destroy
-    @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
-    @comment.destroy
+    if permit_to_delete?
+      @comment.destroy
+    end
     redirect_to post_path(@post)
   end
 
@@ -17,6 +22,21 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:name, :comment_text)
+  end
+
+  def find_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def permit_to_delete?
+    @comment = @post.comments.find(params[:id])
+    @user_id = current_user.id
+
+    if @comment.user_id == @user_id
+      true
+    else
+      false
+    end
   end
 
 end
