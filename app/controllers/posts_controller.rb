6425ebc015c60b0abe3d14a95f868416aa590efd
Nayeem Mahmood <alive.dew@gmail.com>
2020-12-15 10:48:43 +0600

    user authenticated before creating, editing and deleting posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
 
   def index
     @posts = Post.all.order("updated_at DESC")
