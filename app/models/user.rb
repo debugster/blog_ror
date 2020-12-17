5901a5035db50e8b1377efa8cf36b1b39f05f0b8
Nayeem Mahmood <alive.dew@gmail.com>
2020-12-17 11:40:16 +0600

    user can login with handle/email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+  attr_accessor :login
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,4 +9,15 @@ class User < ApplicationRecord
 
   has_many :posts
   has_many :comments
+
+  def self.find_for_database_authentication warden_condition
+    conditions = warden_condition.dup
+    login = conditions.delete(:login)
+    where(conditions).where(
+      [
+        "lower(handle) = :value OR lower(email) = :value",
+        { value: login.strip.downcase }
+      ]
+    ).first
+  end
 end
